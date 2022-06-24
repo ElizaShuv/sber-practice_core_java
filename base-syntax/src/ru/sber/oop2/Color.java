3875ba46caa5526aaadc271d11b80c6bf787e98f
ElizaShuv <107874508+ElizaShuv@users.noreply.github.com>
2022-06-24 13:33:14 +0300

    Delete base-syntax/src/ru/sber/oop2 directory

--- base-syntax/src/ru/sber/oop2/Color.java
+++
@@ -1,9 +0,0 @@
-package ru.sber.oop2;
-
-public enum Color {
-    BLUE,
-    YELLOW,
-    PINK,
-    RED,
-    BLACK
-}
--- base-syntax/src/ru/sber/oop2/Drawable.java
+++
@@ -1,7 +0,0 @@
-package ru.sber.oop2;
-
-public interface Drawable {
-     void draw();
-     void draw(Color color);
-
-}
