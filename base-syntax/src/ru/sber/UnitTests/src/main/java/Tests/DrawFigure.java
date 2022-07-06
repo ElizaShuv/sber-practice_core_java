e96c5dee9f06ed94e216a182fc56f7adad1ec0af
ElizaShuv <107874508+ElizaShuv@users.noreply.github.com>
2022-07-06 22:34:49 +0300

    Выполнено задание по Unit-тестированию

---
+++ base-syntax/src/ru/sber/UnitTests/src/main/java/Tests/DrawFigure.java
@@ -0,0 +1,22 @@
+package Tests;
+
+public class DrawFigure  {
+
+    String color;
+
+    public String draw(String color, String figureName){
+        return String.format("%s %s нарисован", color, figureName);
+    }
+
+    public String delete(){
+        return "Фигура удалена";
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+}
