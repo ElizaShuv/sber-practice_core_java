38dc3536b9fdf4e6f0f1ab96b9ce3b7ba064fd82
ElizaShuv <107874508+ElizaShuv@users.noreply.github.com>
2022-07-11 02:39:27 +0300

    Add files via upload

--- base-syntax/src/ru/sber/Lambdas/src/main/java/lambdas/LambdaProgram.java
+++ base-syntax/src/ru/sber/Lambdas/src/main/java/lambdas/LambdaProgram.java
@@ -2,8 +2,8 @@ package lambdas;
 
 public class LambdaProgram {
 
-    public static void getMessage(ItFunctionalInterface message) {
-        message.out();
+    public static String getMessage(ItFunctionalInterface message) {
+        return message.out();
     }
 
     public static String getMessage(MessageFunctionInterface<String> message) {
@@ -20,44 +20,17 @@ public class LambdaProgram {
 
         getMessage(() -> {
             System.out.println(ItFunctionalInterface.S);
+            return ItFunctionalInterface.S;
         });
 
         getMessage((s) -> {
-            System.out.println(s+str1);
-            return s+str1;
+            System.out.println(s + str1);
+            return s + str1;
         });
-        changeWord(str1, str2, (s1,s2) -> {
+        changeWord(str1, str2, (s1, s2) -> {
             String s3 = s2 + " " + s1;
             System.out.println(s3);
             return s3;
         });
-
-        ItFunctionalInterface itFunctionalInterface = new ItFunctionalInterface() {
-            @Override
-            public void out() {
-                System.out.println("It function interface");
-            }
-        };
-        System.out.println();
-        itFunctionalInterface.out();
-
-        MessageFunctionInterface messageFunctionInterface = new MessageFunctionInterface() {
-            @Override
-            public String get(String mes) {
-                return MessageFunctionInterface.S + mes;
-            }
-        };
-
-        System.out.println(messageFunctionInterface.get(str1));
-
-        ChangeFunctionInterface changeFunctionInterface = new ChangeFunctionInterface() {
-            @Override
-            public String str(String s1, String s2) {
-                return s2 + " " + s1;
-            }
-        };
-
-        System.out.println(changeFunctionInterface.str(str1, str2));
-
     }
 }
