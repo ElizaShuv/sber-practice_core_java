fafa64964ebd19113e1d80c9773d8322049c283f
ElizaShuv <107874508+ElizaShuv@users.noreply.github.com>
2022-07-11 02:40:21 +0300

    Изменено тестирование

--- base-syntax/src/ru/sber/Lambdas/src/test/java/lambdas/TestLambdaProgram.java
+++ base-syntax/src/ru/sber/Lambdas/src/test/java/lambdas/TestLambdaProgram.java
@@ -1,31 +1,47 @@
 package lambdas;
 
 import org.junit.Test;
-import static org.mockito.Mockito.*;
 
-    public class TestLambdaProgram {
-        private final static String TEST_STR1 = "Hello";
-        private final static String TEST_STR2 = "World";
-        private final static String TEST_STR = "Hello World";
-        private final static String TEST_GET = "Message: World";
+import static org.junit.Assert.assertEquals;
 
-        @Test
-        public void firstTest() {
-            ChangeFunctionInterface changeFunctionInterface = mock(ChangeFunctionInterface.class);
-            when(changeFunctionInterface.str(TEST_STR2,TEST_STR1)).thenReturn(TEST_STR);
 
-            MessageFunctionInterface messageFunctionInterface = mock(MessageFunctionInterface.class);
-            when(messageFunctionInterface.get(TEST_STR2)).thenReturn(TEST_GET);
-        }
+public class TestLambdaProgram {
+    private final static String TEST_STR1 = "Hello";
+    private final static String TEST_STR2 = "World";
+    private final static String TEST_STR = "Hello World";
+    private final static String TEST_GET = "Message: World";
+    private final static String TEST_OUT = "It function interface";
 
-        @Test(expected = IllegalStateException.class)
-        public void secondTest() {
-            ChangeFunctionInterface changeFunctionInterface = mock(ChangeFunctionInterface.class);
-            when(changeFunctionInterface.str(anyString(),anyString())).thenThrow(new IllegalStateException());
-            changeFunctionInterface.str(TEST_STR2, TEST_STR1);
+    @Test
+    public void firstTest() {
+        ItFunctionalInterface itFunctionalInterface = new ItFunctionalInterface() {
+            @Override
+            public String out() {
+                return "It function interface";
+            }
+        };
+        assertEquals(TEST_OUT, itFunctionalInterface.out());
+    }
 
-            MessageFunctionInterface messageFunctionInterface = mock(MessageFunctionInterface.class);
-            when(messageFunctionInterface.get(anyString())).thenThrow(new IllegalStateException());
-            messageFunctionInterface.get(TEST_STR2);
-        }
+    @Test
+    public void secondTest() {
+        MessageFunctionInterface messageFunctionInterface = new MessageFunctionInterface() {
+            @Override
+            public String get(String mes) {
+                return MessageFunctionInterface.S + mes;
+            }
+        };
+        assertEquals(TEST_GET, messageFunctionInterface.get(TEST_STR2));
+    }
+
+    @Test
+    public void thirdTest() {
+        ChangeFunctionInterface changeFunctionInterface = new ChangeFunctionInterface() {
+            @Override
+            public String str(String s1, String s2) {
+                return s2 + " " + s1;
+            }
+        };
+        assertEquals(TEST_STR, changeFunctionInterface.str(TEST_STR2, TEST_STR1));
+    }
 }
