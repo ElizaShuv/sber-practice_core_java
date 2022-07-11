fa24671c13ef2bbce3d00d395a79e2796bf48aab
ElizaShuv <107874508+ElizaShuv@users.noreply.github.com>
2022-07-11 10:29:04 +0300

    Изменено тестирование

--- base-syntax/src/ru/sber/Lambdas/src/main/java/lambdas/LambdaProgram.java
+++ base-syntax/src/ru/sber/Lambdas/src/main/java/lambdas/LambdaProgram.java
@@ -14,23 +14,4 @@ public class LambdaProgram {
         word.str(s1, s2);
     }
 
-    public static void main(String[] args) {
-        String str1 = "World";
-        String str2 = "Hello";
-
-        getMessage(() -> {
-            System.out.println(ItFunctionalInterface.S);
-            return ItFunctionalInterface.S;
-        });
-
-        getMessage((s) -> {
-            System.out.println(s + str1);
-            return s + str1;
-        });
-        changeWord(str1, str2, (s1, s2) -> {
-            String s3 = s2 + " " + s1;
-            System.out.println(s3);
-            return s3;
-        });
-    }
 }
